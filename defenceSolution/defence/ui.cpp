6b534b056e73c5dd8d13ae8be2a877433f0984a6
markus456 <markus456@nomail.com>
2014-02-19 11:27:56 +0200

    Lisätty metodeja ja SDL_ttf -kirjasto
    Lisättiin SDL_ttf -kirjasto fonttitiedostojen kanssa.
    Lisättiin konstruktoreita eri tarkoituksia varten.
    Settereitä ja gettereitä lisätty.

--- defenceSolution/defence/ui.cpp
+++ defenceSolution/defence/ui.cpp
@@ -1,21 +1,60 @@
 #include "ui.h"
-Button::Button(){
-	_x = 0;
-	_y = 0;
-	_w = 0;
-	_h = 0;
+void Button::render(SDL_Surface* dest){
+	SDL_BlitSurface(_surface,nullptr,dest,nullptr);
 }
-Button::Button(float x, float y, float width, float height){
-	_x = x;
-	_y = y;
-	_w = width;
-	_h = height;
-
-}
-void Button::render(){
-
-}
-
+//kutsuu asetettua funktiopointteria, jos x ja y ovat nappulan sisäpuolella
 void Button::onClick(float x, float y){
+	if(isInside(x,y)){
+		_f_ptr();
+	}
+}
+bool Button::isInside(int x, int y){
+if(_x<x&&_x+_w>x&&_y<y&&_y+_h>y){
+	return true;	
+}
+return false;
+}
 
+void Button::setX(float x){
+	_x = x;
+}
+
+float Button::getX()const{
+	return _x;
+}
+
+void Button::setY(float y){
+	_y = y;
+}
+
+float Button::getY()const{
+	return _y;
+}
+
+void Button::setW(float w){
+	_w = w;
+}
+
+float Button::getW()const{
+	return _w;
+}
+
+void Button::setH(float h){
+	_h = h;
+}
+
+float Button::getH()const{
+	return _h;
+}
+
+void Button::setText(const char* text){
+	_text = text;
+}
+
+std::string Button::getText()const{
+	return _text;
+}
+
+void Button::setCallback(void(*function_pointer)(void)){
+	_f_ptr = function_pointer;
 }
--- defenceSolution/defence/ui.h
+++ defenceSolution/defence/ui.h
@@ -1,12 +1,73 @@
 #ifndef UI_HEADER_GUARD
 #define UI_HEADER_GUARD
+#include "include\SDL.h"
+#include "include\SDL_ttf.h"
+#include <string>
 class Button{
 protected:
 	float _x,_y,_w,_h;
+	std::string _text;
+	void (*_f_ptr)(void);
+	SDL_Surface* _surface;
+	TTF_Font* _font;
 public:
-	Button();
-	Button(float,float,float,float);
-	virtual void render();
+	Button():_x(0), _y(0),_w(0),_h(0),_text(nullptr),_f_ptr(nullptr),_surface(nullptr){}
+
+	//konstruktori pelk‰ll‰ paikkatiedolla
+	Button(float x,float y,float w,float h):_x(x), _y(y),_w(w),_h(h),_text(nullptr),_f_ptr(nullptr){}
+
+	//konstruktori paikkatiedolla ja tekstill‰
+	Button(float x,float y,float w,float h,const char* text):_x(x), _y(y),_w(w),_h(h),_text(text),_f_ptr(nullptr),_surface(nullptr){
+		TTF_Init();
+		_font = TTF_OpenFont("FreeMonoBold.ttf",20);
+		SDL_Color color;
+		color.r = 255;
+		color.g = 255;
+		color.b= 255;
+		color.a = 255;
+		SDL_BlitSurface(TTF_RenderText_Blended_Wrapped(_font,_text.c_str(),color,_w),nullptr,_surface,nullptr);
+	}
+
+	//konstruktori paikkatiedolla, tekstill‰ ja funktiolla
+	Button(float x,float y,float w,float h,const char* text, void(*function_pointer)(void)):_x(x), _y(y),_w(w),_h(h),_text(text),_f_ptr(function_pointer),_surface(nullptr){
+		TTF_Init();
+		_font = TTF_OpenFont("FreeMonoBold.ttf",20);
+		SDL_Color color;
+		color.r = 255;
+		color.g = 255;
+		color.b= 255;
+		color.a = 255;
+		SDL_BlitSurface(TTF_RenderText_Blended_Wrapped(_font,_text.c_str(),color,_w),nullptr,_surface,nullptr);
+	}
+
+	/**Konstruktori paikkatiedolla, tekstill‰, funktiolla ja taustakuvalla
+	*Lataa kuvan tiedostosta, jonka j‰lkeen piirt‰‰ siihen tekstin
+	*/
+	Button(float x,float y,float w,float h,const char* text, void(*function_pointer)(void),const char* image):_x(x), _y(y),_w(w),_h(h),_text(text),_f_ptr(function_pointer){
+		TTF_Init();
+		_surface = SDL_LoadBMP(image);
+		_font = TTF_OpenFont("FreeMonoBold.ttf",20);
+		SDL_Color color;
+		color.r = 255;
+		color.g = 255;
+		color.b= 255;
+		color.a = 255;
+		SDL_BlitSurface(TTF_RenderText_Blended_Wrapped(_font,_text.c_str(),color,_w),nullptr,_surface,nullptr);
+	}
+
+	virtual void render(SDL_Surface*);
 	virtual void onClick(float,float);
+	virtual bool isInside(int,int);
+	void setX(float);
+	float getX()const;
+	void setY(float);
+	float getY()const;
+	void setW(float);
+	float getW()const;
+	void setH(float);
+	float getH()const;
+	void setText(const char*);
+	std::string getText()const;
+	void setCallback(void(*function_pointer)(void));
 };
 #endif
